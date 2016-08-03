41cc71b3d3abb0e656c5513e9ce849f517ee7076
Chinnasamy <echinnasamy@paypalcorp.com>
2016-08-03 17:25:47 +0530

    added getMax and getMin

---
+++ odometer.java
@@ -0,0 +1,19 @@
+
+	int getMinMax(int order,int flag){
+		int minNums = {12,123,1234,12345,123456,1234567,12345678};
+		int maxNums = {98,789,6789,56789,456789,3456789,23456789};
+		if(flag == 1){
+			return minNums[order-2];
+		}
+		else{
+			return maxNums[order-2];
+		}
+	}
+	
+	int getMin(int order){
+		return getMinMax(order,1);
+	}
+	
+	int getMax(int order){
+		return getMinMax(order,2);
+	}

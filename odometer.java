cf5e9285953b0332d79116470949fe9874300d5a
Athreya <aathreya@paypalcorp.com>
2016-08-03 17:46:21 +0530

    Added functions.

--- odometer.java
+++ odometer.java
@@ -1,3 +1,53 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Odometer{
+
+	
+	public static int getnNext(int number,int diff,int order)
+	{
+	    
+	    for(int i = 0;i<diff;i++)
+	    {
+	       number = getNext(number,order);
+	    }
+	    return number;
+	}
+	
+	public static int getNext(int number,int order)
+	{
+	    int temp = number;
+	    if(temp == getMax(order))return getMin(order);
+	    temp++;
+	    while(!isAscending(temp))
+	    {
+	        temp = getNext(temp);
+	    }
+	    return temp;
+	}
+	
+	public static int getnPrevious(int number,int diff,int order)
+	{
+	    for(int i=0;i<diff;i++)
+	    {
+	        number = getPrevious(number,order);
+	    }
+	    return number;
+	}
+	
+	public static int getPrevious(int number,int order)
+	{
+	    int temp = number;
+	    if(temp == getMin(order))return getMax(order);
+	    temp--;
+	    while(!isAscending(temp))
+	    {
+	        temp = getPrevious(temp);
+	    }
+	    return temp;
+	}
 	
 	public static int getMinMax(int order,int flag){
 		int[] minNums = {12,123,1234,12345,123456,1234567,12345678};
@@ -16,4 +66,5 @@
 	
 	public static int getMax(int order){
 		return getMinMax(order,2);
+	}
 	}
